b99be5dd1409713f11497abd3e0de3fb5fdd619d
sd880428 <sd660428@gmail.com>
2023-03-18 01:01:55 +0800

    feat: 新增tweet seeder

---
+++ seeders/20230317162608-seed-tweet.js
@@ -0,0 +1,25 @@
+'use strict'
+const faker = require('faker')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+
+    for (let i = 0; i < users.length; i++) {
+      await queryInterface.bulkInsert('Tweets',
+        Array.from({ length: 10 }, () => ({
+          User_id: users[i].id,
+          image: `https://loremflickr.com/320/240/?random=${Math.random() * 100}`,
+          description: faker.lorem.sentence(5),
+          created_at: new Date(),
+          updated_at: new Date()
+        }))
+      )
+    }
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Tweets', {})
+  }
+}

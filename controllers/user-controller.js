22e2d7d64a639759ed3e9d2db238022d7fb537ad
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-03-20 02:52:05 +0800

    feat: user function - get user homepage tweets

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,11 +7,51 @@ const userController = {
         raw: true
       })
       user.tweetsCounts = await Tweet.count({ where: { userId: id } })
-      user.followersCounts = await Followship.count({ where: { followingId: id } })
-      user.followingsCounts = await Followship.count({ where: { followerId: id } })
+      user.followersCounts = await Followship.count({
+        where: { followingId: id }
+      })
+      user.followingsCounts = await Followship.count({
+        where: { followerId: id }
+      })
       user.currentUser = id === req.id
       const response = {
         data: user
+      }
+      res.status(200).json(response)
+    } catch (err) {
+      next(err)
+    }
+  },
+  getUserTweets: async (req, res, next) => {
+    try {
+      // const userId = req.user.id
+      const userId = req.user || 3
+      const followings = await Followship.findAll({
+        where: { followerId: userId },
+        attributes: ['followingId'],
+        raw: true
+      })
+      // Set可以拿掉 目前種子資料難以避免重複追蹤
+      const showIds = [...new Set(followings.map(e => e.followingId))]
+      showIds.push(userId)
+      const tweets = await Tweet.findAll({
+        where: { userId: showIds },
+        include: [
+          { model: User, attributes: ['name', 'account'] },
+          { model: Reply },
+          { model: Like }
+        ],
+        order: [['createdAt', 'DESC']], // or ['id', 'DESC'] 因為理論上id越後面越新
+        nest: true
+      })
+      const result = tweets.map(e => {
+        const temp = e.toJSON()
+        temp.Replies = temp.Replies.length
+        temp.Likes = temp.Likes.length
+        return temp
+      })
+      const response = {
+        data: result
       }
       res.status(200).json(response)
     } catch (err) {

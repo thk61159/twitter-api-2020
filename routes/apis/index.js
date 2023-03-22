7b90198c90ddf2ee0fc7679f7b9d4aafb188adb3
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-03-22 22:05:17 +0800

    refactor: user route and authentication route rearrange

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,9 +1,30 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
 const users = require('./user')
 const admin = require('./admin')
+const passport = require('passport')
+const { authenticated } = require('../../middleware/api-auth')
+const userController = require('../../controllers/user-controller')
+const { apiErrorHandler } = require('../../middleware/error-handler')
 
-router.use('/users', users)
+router.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json')
+  next()
+})
+router.post('/users/', userController.signUp)
+router.post('/users/test-token', userController.userVerify)
+router.post(
+  '/users/login',
+  passport.authenticate('local', {
+    session: false,
+    failureMessage: true,
+    failWithError: true
+  }),
+  userController.signIn,
+  userController.signInFail
+)
+
+router.use('/users', authenticated, users)
 router.use('/admin', admin)
 
+router.use('/', apiErrorHandler)
 module.exports = router
